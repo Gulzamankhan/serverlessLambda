8ca52148fa1b435f918e974194c2adaa6abbe5af
Gulzamankhan <gzk1gul@gmail.com>
2018-01-09 15:17:12 +0500

    GMO_RINGS-POC_CI_CD_Pipeline

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'GMO_RINGS_POC The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'GMO_RINGS-POC_CI_CD_Pipeline The time in Los Angeles is: ' + currentTime.toString(),
     });
 };
